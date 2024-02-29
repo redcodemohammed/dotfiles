2ef778a2ac13b0065e312aaee971e51808c8974c
redcodemohammed <redcodemohammed@gmail.com>
2024-02-29 17:16:58 +0300

    added nvim which-key plugin

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,28 +1,28 @@
 local lsp = require('lsp-zero')
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({buffer = bufnr})
 end)
 
 lsp.setup_servers({
-	'tsserver',
-	'rust_analyzer',
-	'eslint',
-	'lua_ls',
-	"volar",
-	"tailwindcss",
-	"jedi_language_server",
-	"prismals",
-	"marksman",
-	"jsonls",
-	"html",
-	"emmet_language_server",
-	"dockerls",
-	"docker_compose_language_service",
-	"cssls",
-	"bashls"
+    'tsserver',
+    'rust_analyzer',
+    'eslint',
+    'lua_ls',
+    "volar",
+    "tailwindcss",
+    "jedi_language_server",
+    "prismals",
+    "marksman",
+    "jsonls",
+    "html",
+    "emmet_language_server",
+    "dockerls",
+    "docker_compose_language_service",
+    "cssls",
+    "bashls",
 
 })
 
@@ -46,41 +46,62 @@ cmp.setup({
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {
-		'tsserver',
-		'rust_analyzer',
-		'eslint',
-		'lua_ls',
-		"volar",
-		"tailwindcss",
-		"jedi_language_server",
-		"prismals",
-		"marksman",
-		"jsonls",
-		"html",
-		"emmet_language_server",
-		"dockerls",
-		"docker_compose_language_service",
-		"cssls",
-		"bashls"	
-	},
-	handlers = { lsp.default_setup,},
+    ensure_installed = {
+        'tsserver',
+        'rust_analyzer',
+        'eslint',
+        'lua_ls',
+        "volar",
+        "tailwindcss",
+        "jedi_language_server",
+        "prismals",
+        "marksman",
+        "jsonls",
+        "html",
+        "emmet_language_server",
+        "dockerls",
+        "docker_compose_language_service",
+        "cssls",
+        "bashls",
+    },
+    handlers = { lsp.default_setup,},
 })
 
+-- setup for dart
+require("lspconfig").dartls.setup({
+    cmd = { "dart", "language-server", "--protocol=lsp" },
+    filetypes = { "dart" },
+    init_options = {
+        closingLabels = true,
+        flutterOutline = true,
+        onlyAnalyzeProjectsWithOpenFiles = true,
+        outline = true,
+        suggestFromUnimportedLibraries = true,
+    },
+    -- root_dir = root_pattern("pubspec.yaml"),
+    settings = {
+        dart = {
+            completeFunctionCalls = true,
+            showTodos = true,
+        },
+    },
+    on_attach = function(client, bufnr)
+    end,
+})
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-i>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-i>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 
