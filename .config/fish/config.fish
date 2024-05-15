5b067634893b0c26bd105143bd07711a0cc80505
redcodemohammed <redcodemohammed@gmail.com>
2024-05-15 10:15:25 +0300

    Update Alacritty theme and configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,10 @@ if status is-interactive
 end
 
 # set alias
-alias ex 'exa --long --header --git'
-alias tree 'exa --header -a --tree -I .git'
+alias ex 'exa --long --header --git --icons'
+alias tree 'exa --header -a --tree -I .git --icons'
 alias cat 'bat'
+alias ls 'ex'
 
 # disable greeting
 set -g fish_greeting
@@ -20,3 +21,18 @@ zoxide init fish | source
 fish_add_path /home/mohammed/.spicetify
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/mohammed/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# android
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x PATH $ANDROID_HOME/emulator $PATH
+set -x PATH $ANDROID_HOME/tools $PATH
+set -x PATH $ANDROID_HOME/tools/bin $PATH
+set -x PATH $ANDROID_HOME/platform-tools $PATH
+# android end
